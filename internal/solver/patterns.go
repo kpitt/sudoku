9063d9887977e7b9247b5d72db7d6b3af5789329
Kenny Pitt <kenny.pitt@gmail.com>
2025-07-26 12:26:37 -0400

    Short-circuit checks if candidates are invalidated
    If a check starts by selecting a set of viable candidates that meet a
    certain set of requirements, then any change to the board state could
    invalidate the remaining candidates.  This could result in incorrect
    matches, so it is best to short-circuit after the first successful
    candidate.  Otherwise, we would need to keep re-checking the criteria
    for each candidate, which would complicate the code and counteract any
    performance advantage we might have gained by checking all the
    candidates in one loop.
    
    After short-circuiting, we can either continue with any additional
    checks which select a fresh set of candidates, or we can just return
    to the start of the solver loop.  Any candidates that were skipped
    will be picked up in a later solver pass if still needed.

--- internal/solver/patterns.go
+++ internal/solver/patterns.go
@@ -8,13 +8,19 @@ import (
 )
 
 // ***** IMPORTANT NOTE *****
-// If you are combining multiple boolean checks and you want them all to run
-// regardless of whether other checks succeed or fail, then each check **MUST**
-// come before the first `||` operator in the combining expression.  This
-// implies that a separate combining statement is needed for each check, and
-// the accumulator variable must come **AFTER** the check expression.  The OR
-// operator (`||`) will short-circuit after the first `true` expression, so any
-// check that follows an OR operator might not run.
+//
+// When processing a check against a set of viable candidates, _always_
+// short-circuit the remaining checks after making a change that could
+// invalidate the remaining candidates.  Checks should not be combined in a
+// single pass unless the checks are completely independent.  If it isn't
+// clear whether or not the checks are independent, go ahead and short-ciruit,
+// and any additional candidates will get checked in the next solver pass.
+//
+// For checks that should _not_ short-circuit, be careful when using OR
+// expressions to combine the results.  The safest approach is to use an
+// accumulator variable and the pattern `found = check(...) || found` for each
+// check.  The `||` operator will short-circuit after the first term that
+// evalues to `true`, so only the first term is guaranteed to be evaluated.
 
 // findHiddenSingles locks the value of any cells that match the "Hidden Single"
 // pattern.  A "Hidden Single" is the only cell that contains a particular
@@ -32,16 +38,15 @@ func (s *Solver) findHiddenSingles() bool {
 
 func (s *Solver) checkHiddenSinglesForGroup(g *Group) bool {
 	pattern := fmt.Sprintf("Hidden Single (%s)", g.GroupType)
-	found := false
 	for val, locs := range g.Unsolved {
 		if locs.Size() == 1 {
 			index := locs.Values()[0]
 			cell := g.Cells[index]
 			s.LockValue(cell.Row, cell.Col, val, pattern)
-			found = true
+			return true
 		}
 	}
-	return found
+	return false
 }
 
 func (s *Solver) findNakedPairs() bool {
@@ -56,24 +61,23 @@ func (s *Solver) findNakedPairs() bool {
 }
 
 func (s *Solver) checkNakedPairsForGroup(g *Group) bool {
-	found := false
-	candidates := make(map[int]*set.Set[int8])
+	values := make(map[int]*set.Set[int8])
 	for i, c := range g.Cells {
 		// Collect a map of all locations with exactly 2 candidate values.
 		if c.NumCandidates() == 2 {
-			candidates[i] = c.Candidates
+			values[i] = c.Candidates
 		}
 	}
-	if len(candidates) < 2 {
+	if len(values) < 2 {
 		// We need at least 2 candidate values to have a pair.
 		return false
 	}
 
-	locs := mapKeys(candidates)
+	locs := mapKeys(values)
 	for i := 0; i < len(locs)-1; i++ {
 		for j := i + 1; j < len(locs); j++ {
 			a, b := locs[i], locs[j]
-			valueSet := set.Union(candidates[a], candidates[b])
+			valueSet := set.Union(values[a], values[b])
 			if valueSet.Size() != 2 {
 				// If the union of the location sets does not have exactly 2 elements, then
 				// this is not a naked pair.
@@ -81,11 +85,13 @@ func (s *Solver) checkNakedPairsForGroup(g *Group) bool {
 			}
 
 			locSet := set.NewSet(a, b)
-			found = s.eliminateFromOtherLocs(g, valueSet, locSet, "Naked Pair") || found
+			if s.eliminateFromOtherLocs(g, valueSet, locSet, "Naked Pair") {
+				return true
+			}
 		}
 	}
 
-	return found
+	return false
 }
 
 // eliminateFromOtherLocs removes the candidates listed in values from all
@@ -108,6 +114,7 @@ func (s *Solver) eliminateFromOtherLocs(
 			}
 		}
 	}
+
 	return found
 }
 
@@ -129,7 +136,6 @@ func (s *Solver) checkLockedCandidatesForRowCol(g *Group) bool {
 		return l.Size() <= 3
 	})
 
-	found := false
 	for val, locs := range candidates {
 		valueSet := set.NewSet(val)
 		cells := g.cellsFromLocs(locs.Values())
@@ -139,11 +145,15 @@ func (s *Solver) checkLockedCandidatesForRowCol(g *Group) bool {
 				return hr*3 + hc
 			})
 			locSet := set.NewSet(houseCells...)
-			found = s.eliminateFromOtherLocs(
-				s.houseGroups[house], valueSet, locSet, "Locked Candidate") || found
+			if s.eliminateFromOtherLocs(
+				s.houseGroups[house], valueSet, locSet, "Locked Candidate") {
+
+				return true
+			}
 		}
 	}
-	return found
+
+	return false
 }
 
 func (s *Solver) findPointingTuples() bool {
@@ -163,7 +173,6 @@ func (s *Solver) checkPointingTuplesForHouse(g *Group) bool {
 		return l.Size() <= 3
 	})
 
-	found := false
 	for val, locs := range candidates {
 		valueSet := set.NewSet(val)
 		cells := g.cellsFromLocs(locs.Values())
@@ -172,19 +181,26 @@ func (s *Solver) checkPointingTuplesForHouse(g *Group) bool {
 				return c.Col
 			})
 			locSet := set.NewSet(cols...)
-			found = s.eliminateFromOtherLocs(
-				s.rowGroups[row], valueSet, locSet, "Pointing Tuple") || found
+			if s.eliminateFromOtherLocs(
+				s.rowGroups[row], valueSet, locSet, "Pointing Tuple") {
+
+				return true
+			}
 		}
 		if col, ok := g.sharedCol(locs); ok {
 			rows := transformSlice(cells, func(c *board.Cell) int {
 				return c.Row
 			})
 			locSet := set.NewSet(rows...)
-			found = s.eliminateFromOtherLocs(
-				s.colGroups[col], valueSet, locSet, "Pointing Tuple") || found
+			if s.eliminateFromOtherLocs(
+				s.colGroups[col], valueSet, locSet, "Pointing Tuple") {
+
+				return true
+			}
 		}
 	}
-	return found
+
+	return false
 }
 
 func (s *Solver) findHiddenPairs() bool {
@@ -199,20 +215,19 @@ func (s *Solver) findHiddenPairs() bool {
 }
 
 func (s *Solver) checkHiddenPairsForGroup(g *Group) bool {
-	candidates := filterMap(g.Unsolved, func(_ int8, l LocSet) bool {
+	locs := filterMap(g.Unsolved, func(_ int8, l LocSet) bool {
 		return l.Size() == 2
 	})
-	if len(candidates) < 2 {
+	if len(locs) < 2 {
 		// We need at least 2 candidate values to have a pair.
 		return false
 	}
 
-	found := false
-	values := mapKeys(candidates)
+	values := mapKeys(locs)
 	for i := 0; i < len(values)-1; i++ {
 		for j := i + 1; j < len(values); j++ {
 			x, y := values[i], values[j]
-			locSet := set.Union(candidates[x], candidates[y])
+			locSet := set.Union(locs[x], locs[y])
 			if locSet.Size() != 2 {
 				// If the union of the location sets does not have exactly 2 elements, then
 				// this is not a hidden pair.
@@ -220,10 +235,13 @@ func (s *Solver) checkHiddenPairsForGroup(g *Group) bool {
 			}
 
 			valueSet := set.NewSet(x, y)
-			found = s.eliminateOtherValues(g, valueSet, locSet, "Hidden Pair") || found
+			if s.eliminateOtherValues(g, valueSet, locSet, "Hidden Pair") {
+				return true
+			}
 		}
 	}
-	return found
+
+	return false
 }
 
 // eliminateOtherValues removes candidates that are not listed in values
@@ -243,6 +261,7 @@ func (s *Solver) eliminateOtherValues(
 			}
 		}
 	}
+
 	return found
 }
 
@@ -258,25 +277,24 @@ func (s *Solver) findNakedTriples() bool {
 }
 
 func (s *Solver) checkNakedTriplesForGroup(g *Group) bool {
-	found := false
-	candidates := make(map[int]*set.Set[int8])
+	values := make(map[int]*set.Set[int8])
 	for i, c := range g.Cells {
 		// Collect a map of all locations with either 2 or 3 candidate values.
 		if c.NumCandidates() == 2 || c.NumCandidates() == 3 {
-			candidates[i] = c.Candidates
+			values[i] = c.Candidates
 		}
 	}
-	if len(candidates) < 3 {
+	if len(values) < 3 {
 		// We need at least 3 candidate values to have a triple.
 		return false
 	}
 
-	locs := mapKeys(candidates)
+	locs := mapKeys(values)
 	for i := 0; i < len(locs)-2; i++ {
 		for j := i + 1; j < len(locs)-1; j++ {
 			for k := j + 1; k < len(locs); k++ {
 				a, b, c := locs[i], locs[j], locs[k]
-				valueSet := set.Union(candidates[a], candidates[b], candidates[c])
+				valueSet := set.Union(values[a], values[b], values[c])
 				if valueSet.Size() != 3 {
 					// If the union of the location sets does not have exactly 3 elements, then
 					// this is not a naked triple.
@@ -284,13 +302,14 @@ func (s *Solver) checkNakedTriplesForGroup(g *Group) bool {
 				}
 
 				locSet := set.NewSet(a, b, c)
-				found = s.eliminateFromOtherLocs(
-					g, valueSet, locSet, "Naked Triple") || found
+				if s.eliminateFromOtherLocs(g, valueSet, locSet, "Naked Triple") {
+					return true
+				}
 			}
 		}
 	}
 
-	return found
+	return false
 }
 
 func (s *Solver) findXWings() bool {
@@ -300,31 +319,32 @@ func (s *Solver) findXWings() bool {
 
 func (s *Solver) findHiddenTriples() bool {
 	printChecking("Hidden Triple")
-	found := false
 	for i := range 9 {
-		found = s.checkHiddenTriplesForGroup(s.rowGroups[i]) || found
-		found = s.checkHiddenTriplesForGroup(s.colGroups[i]) || found
-		found = s.checkHiddenTriplesForGroup(s.houseGroups[i]) || found
+		if s.checkHiddenTriplesForGroup(s.rowGroups[i]) ||
+			s.checkHiddenTriplesForGroup(s.colGroups[i]) ||
+			s.checkHiddenTriplesForGroup(s.houseGroups[i]) {
+
+			return true
+		}
 	}
-	return found
+	return false
 }
 
 func (s *Solver) checkHiddenTriplesForGroup(g *Group) bool {
-	candidates := filterMap(g.Unsolved, func(_ int8, l LocSet) bool {
+	locs := filterMap(g.Unsolved, func(_ int8, l LocSet) bool {
 		return l.Size() == 2 || l.Size() == 3
 	})
-	if len(candidates) < 3 {
+	if len(locs) < 3 {
 		// We need at least 3 candidate values to have a triple.
 		return false
 	}
 
-	found := false
-	values := mapKeys(candidates)
+	values := mapKeys(locs)
 	for i := 0; i < len(values)-2; i++ {
 		for j := i + 1; j < len(values)-1; j++ {
 			for k := j + 1; k < len(values); k++ {
 				x, y, z := values[i], values[j], values[k]
-				locSet := set.Union(candidates[x], candidates[y], candidates[z])
+				locSet := set.Union(locs[x], locs[y], locs[z])
 				if locSet.Size() != 3 {
 					// If the union of the location sets does not have exactly 3 elements, then
 					// this is not a hidden triple.
@@ -332,12 +352,14 @@ func (s *Solver) checkHiddenTriplesForGroup(g *Group) bool {
 				}
 
 				valueSet := set.NewSet(x, y, z)
-				found = s.eliminateOtherValues(
-					g, valueSet, locSet, "Hidden Triple") || found
+				if s.eliminateOtherValues(g, valueSet, locSet, "Hidden Triple") {
+					return true
+				}
 			}
 		}
 	}
-	return found
+
+	return false
 }
 
 func (s *Solver) findNakedQuadruples() bool {
@@ -347,9 +369,7 @@ func (s *Solver) findNakedQuadruples() bool {
 
 func (s *Solver) findYWings() bool {
 	printChecking("Y Wing")
-	// Collect a list of all cells with exactly 2 candidates.  Note that we'll
-	// still need to re-check each cell later because additional candidates may
-	// get removed along the way.
+	// Collect a list of all cells with exactly 2 candidates.
 	b := s.board
 	var candidates []*board.Cell
 	for r := range 9 {
@@ -365,15 +385,14 @@ func (s *Solver) findYWings() bool {
 		return false
 	}
 
-	found := false
 	// Try each candidate as the base cell, checking it against all of the other
 	// candidates.
 	for _, base := range candidates {
-		if base.NumCandidates() == 2 {
-			found = s.checkYWingsForCell(base, candidates) || found
+		if s.checkYWingsForCell(base, candidates) {
+			return true
 		}
 	}
-	return found
+	return false
 }
 
 func (s *Solver) checkYWingsForCell(
@@ -402,13 +421,9 @@ func (s *Solver) checkYWingsForCell(
 		return false
 	}
 
-	found := false
 	// Check each of the x-cells against each of the y-cells to see if they share
 	// a common 3rd value z.
 	for _, xc := range xCells {
-		if xc.NumCandidates() != 2 {
-			continue
-		}
 		cellVals := xc.CandidateValues()
 		// Set z to the non-x value in the candidate cell.
 		z := cellVals[0]
@@ -420,10 +435,13 @@ func (s *Solver) checkYWingsForCell(
 			if !yc.HasCandidate(z) || seesCell(xc, yc) {
 				continue
 			}
-			found = s.eliminateYWingCells(z, xc, yc) || found
+			if s.eliminateYWingCells(z, xc, yc) {
+				return true
+			}
 		}
 	}
-	return found
+
+	return false
 }
 
 // eliminateYWingCells removes candidate value z from all cells that see both
@@ -477,27 +495,21 @@ func (s *Solver) findHiddenQuadruples() bool {
 }
 
 func (s *Solver) checkHiddenQuadruplesForGroup(g *Group) bool {
-	found := false
-	candidates := filterMap(g.Unsolved, func(_ int8, l LocSet) bool {
+	locs := filterMap(g.Unsolved, func(_ int8, l LocSet) bool {
 		return l.Size() == 2 || l.Size() == 3 || l.Size() == 4
 	})
-	if len(candidates) < 4 {
+	if len(locs) < 4 {
 		// We need at least 4 candidate values to have a quadruple.
 		return false
 	}
 
-	values := mapKeys(candidates)
+	values := mapKeys(locs)
 	for i := 0; i < len(values)-3; i++ {
 		for j := i + 1; j < len(values)-2; j++ {
 			for k := j + 1; k < len(values)-1; k++ {
 				for n := k + 1; n < len(values); n++ {
 					w, x, y, z := values[i], values[j], values[k], values[n]
-					locSet := set.Union(
-						candidates[w],
-						candidates[x],
-						candidates[y],
-						candidates[z],
-					)
+					locSet := set.Union(locs[w], locs[x], locs[y], locs[z])
 					if locSet.Size() != 4 {
 						// If the union of the location sets does not have exactly 4 elements, then
 						// this is not a hidden quadruple.
@@ -505,12 +517,15 @@ func (s *Solver) checkHiddenQuadruplesForGroup(g *Group) bool {
 					}
 
 					valueSet := set.NewSet(w, x, y, z)
-					found = s.eliminateOtherValues(
-						g, valueSet, locSet, "Hidden Quadruple") || found
+					if s.eliminateOtherValues(
+						g, valueSet, locSet, "Hidden Quadruple") {
+
+						return true
+					}
 				}
 			}
 		}
 	}
 
-	return found
+	return false
 }
